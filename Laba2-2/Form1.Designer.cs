59f528bfe0b890a1496026597e7aed2aaf5f7244
alyonakkk <73276224+alyonakkk@users.noreply.github.com>
2020-11-03 16:08:34 +0500

    Добавьте файлы проекта.

---
+++ Laba2-2/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Laba2_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(0, 128);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(799, 319);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(297, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(192, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Какую функцию посчитать?";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(443, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Посчитать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(300, 57);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
---
+++ Laba2-2/Form1.cs
@@ -0,0 +1,64 @@
+﻿using LiveCharts;
+using LiveCharts.Defaults;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Laba2_2
+{
+    public partial class Form1 : Form
+    {
+        private List<LibToolscs> list = new List<LibToolscs>();
+        public Form1()
+        {
+            InitializeComponent();
+            string[] pathsDll = Directory.GetFiles("Plugins", "*.dll"); //название папки и расширения, которые надо извлечь
+            foreach (var path in pathsDll)
+            {
+				try
+				{
+					list.Add(new LibToolscs(path)); //добавляет в список
+				}
+				catch { }
+			}
+
+            foreach (var method in list)
+                comboBox1.Items.Add(method.FuncName()); //добавляем функции в комбобокс
+        }
+
+		private void Func()
+        {
+			cartesianChart1.Series.Clear();
+			ChartValues<ObservablePoint> points = new ChartValues<ObservablePoint>();
+			Func<string> funcName = null;
+			Func<double, double> theFunc = null;
+			var name = comboBox1.Text;
+			foreach (var method in from method in list
+								   where name == method.FuncName()
+								   select method)
+			{
+				funcName = method.FuncName;
+				theFunc = method.TheFunc;
+			}
+
+			for (double x = 0; x <= 10; x += 0.5)
+				points.Add(new ObservablePoint(x, theFunc(x)));
+
+			cartesianChart1.Series.Clear();
+			cartesianChart1.Series.Add(new LineSeries
+			{
+				Values = points,
+				Title = funcName()
+			});
+		}
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+			Func();
+		}
+    }
+}
